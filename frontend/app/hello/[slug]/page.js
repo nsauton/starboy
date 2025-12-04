5e9dfba97f6ba56c9151e2b88924d552d983b51a
Nathan Sauton <nsauton@ucsc.edu>
2025-12-03 22:57:01 -0800

    removing dynamic page from hello folder in frontend

--- frontend/app/hello/[slug]/page.js
+++
@@ -1,9 +0,0 @@
-export default async function dynamicPage({ params }) {
-  const { slug } = await params
-
-  return (
-    <div> 
-      <h1>this what was in the url: {slug}</h1>
-    </div>
-  )
-}
